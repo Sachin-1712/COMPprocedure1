9301f34a11f669c49cef4a1421c773f55c27724e
sachin <146246758+Sachin-1712@users.noreply.github.com>
2023-11-23 18:05:45 +0000

    notdone

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -11,26 +11,72 @@ typedef struct {
 
 // Function to tokenize a record
 int tokeniseRecord(char *record, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, ",\t");
-    if (ptr != NULL) {
-        strcpy(date, ptr);
-        ptr = strtok(NULL, ",\t");
-        if (ptr != NULL) {
-            strcpy(time, ptr);
-            ptr = strtok(NULL, ",\t");
-            if (ptr != NULL) {
-                *steps = atoi(ptr);
-                return 0;
+    // Check for an empty line
+    if (record[0] == '\0' || record[0] == '\n') {
+        printf("Error: Empty line found. Skipping.\n");
+        return 1;  // Empty line
+    }
+
+    // Check for the number of tokens
+    int tokenCount = 0;
+    char *ptr = record;
+    while (*ptr != '\0') {
+        if (*ptr == ',' || *ptr == '\t') {
+            tokenCount++;
+        }
+        ptr++;
+    }
+
+    if (tokenCount != 2) {
+        printf("Error: Incorrect number of tokens in line. Skipping.\n");
+        return 1;  // Incorrect number of tokens
+    }
+
+    // Tokenize the record
+    ptr = strtok(record, ",\t");
+    if (ptr == NULL) {
+        printf("Error: Missing date in line. Skipping.\n");
+        return 1;  // Missing date
+    }
+    strcpy(date, ptr);
+
+    ptr = strtok(NULL, ",\t");
+    if (ptr == NULL) {
+        printf("Error: Missing time in line. Skipping.\n");
+        return 1;  // Missing time
+    }
+    strcpy(time, ptr);
+
+    ptr = strtok(NULL, ",\t");
+    if (ptr == NULL) {
+        printf("Error: Missing steps in line. Skipping.\n");
+        return 1;  // Missing steps
+    }
+
+    // Check if steps conversion is successful
+    char *endptr;
+    *steps = strtol(ptr, &endptr, 10);
+
+    if (*endptr != '\0' && *endptr != '\n') {
+        printf("Error: Invalid steps format in line. Skipping.\n");
+        return 1;  // Invalid steps format
+    }
+
+    return 0;  // Successful tokenization
+}
+
+// Function to perform bubble sort on FitnessData array
+void bubbleSort(FitnessData *data, int count) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (data[j].steps < data[j + 1].steps) {
+                // Swap data[j] and data[j+1]
+                FitnessData temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
             }
         }
     }
-    return 1;
-}
-
-
-// Function to compare two FitnessData records for sorting
-int compareRecords(const void *a, const void *b) {
-    return ((FitnessData*)b)->steps - ((FitnessData*)a)->steps;
 }
 
 int main() {
@@ -51,14 +97,16 @@ int main() {
     char line[100];
 
     while (fgets(line, sizeof(line), file) != NULL) {
-        tokeniseRecord(line, data[count].date, data[count].time, &data[count].steps);
-        count++;
+        // Check for errors during tokenization
+        if (tokeniseRecord(line, data[count].date, data[count].time, &data[count].steps) == 0) {
+            count++;
+        }
     }
 
     fclose(file);
 
-    // Sort the data
-    qsort(data, count, sizeof(FitnessData), compareRecords);
+    // Sort the data using bubble sort
+    bubbleSort(data, count);
 
     // Create and write to the output file
     char outputFilename[100];
